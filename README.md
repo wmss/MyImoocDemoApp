e0aac29fb4383c5f5494876fe8604e88f61f58b2
wmss <wmss.wj.true@gmail.com>
2016-06-12 00:53:40 +0800

    add

---
+++ README.md
@@ -0,0 +1,2 @@
+# MyImoocDemoApp
+本工程是用来学习[慕课网](www.imooc.com)的Android课程所建立的项目
---
+++ app/src/main/java/tk/wmss/myimoocdemoapp/SecondCourse/SecondCourseActivity.java
@@ -0,0 +1,25 @@
+package tk.wmss.myimoocdemoapp.SecondCourse;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import tk.wmss.myimoocdemoapp.R;
+
+/**
+ * Created by wmss on 2016/6/11.
+ */
+public class SecondCourseActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_course);
+    }
+
+    public void goToListView(View view){
+        Intent intent = new Intent(SecondCourseActivity.this, ListViewActivity.class);
+        startActivity(intent);
+    }
+
+}
